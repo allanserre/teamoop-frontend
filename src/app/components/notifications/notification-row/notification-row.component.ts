77be481ff83cebbb773e5ae55d26d606ef8d2b40
allan.serre@etu.univ-lehavre.fr <allan.serre@etu.univ-lehavre.fr>
2025-01-28 13:04:07 +0100

    add component notification-row

---
+++ src/app/components/notifications/notification-row/notification-row.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-row',
+  standalone: true,
+  imports: [],
+  templateUrl: './notification-row.component.html',
+  styleUrl: './notification-row.component.scss'
+})
+export class NotificationRowComponent {
+
+}
