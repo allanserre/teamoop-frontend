f7b8df73818890f6bd185ac577a495451b6c5005
LeNelSoN <nelis.valentin@gmail.com>
2025-01-29 17:36:17 +0100

    add icon to classic button

--- src/app/components/classic-button/classic-button.component.ts
+++ src/app/components/classic-button/classic-button.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, output } from '@angular/core';
+import { Component, ElementRef, input, output, AfterContentInit } from '@angular/core';
 import { ButtonColor } from './button.type';
 
 @Component({
@@ -9,17 +9,62 @@ import { ButtonColor } from './button.type';
   templateUrl: './classic-button.component.html',
   styleUrl: './classic-button.component.scss'
 })
-export class ClassicButtonComponent {
+export class ClassicButtonComponent implements AfterContentInit {
   color = input<ButtonColor>('primary');
   filled = input<boolean>(true);
   disabled = input<boolean>(false);
 
   clicked = output<void>();
 
+  hasIcon = false;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterContentInit(): void {
+    setTimeout(() => {
+      this.sanitizeIcons();
+      this.hasIcon = this.detectIcon();
+    });
+  }
+
+  private sanitizeIcons():void {
+    if (!this.elementRef || !this.elementRef.nativeElement) {
+      return;
+    }
+
+    const buttonElement: HTMLElement = this.elementRef.nativeElement;
+
+    const icons: NodeListOf<HTMLElement> = buttonElement.querySelectorAll('i:not([icon])');
+
+    if (icons.length > 1) {
+
+      icons.forEach((icon: HTMLElement, index: number) => {
+        if (index > 0) {
+          icon.remove();
+        } else {
+          icon.setAttribute('icon', '');
+        }
+      });
+    } else if (icons.length === 1) {
+      icons[0].setAttribute('icon', '');
+    }
+  }
+
+  private detectIcon(): boolean {
+    return this.elementRef.nativeElement.querySelector('i[icon]') !== null;
+  }
+
   handleClick(): void {
-    if (!this.disabled) {
+    if (!this.isDisabled) {
       this.clicked.emit();
     }
   }
 
+  get isDisabled(): boolean {
+    return this.disabled();
+  }
+
+  get isFilled(): boolean {
+    return this.filled();
+  }
 }
