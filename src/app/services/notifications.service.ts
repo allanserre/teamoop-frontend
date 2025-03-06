e9879f540269833b8e0d5639ce6460d5ee3f936c
Allan Serre <aserre.pro14@gmail.com>
2025-03-07 00:13:03 +0100

    better mock to adapt to specific case

--- src/app/services/notifications.service.ts
+++ src/app/services/notifications.service.ts
@@ -10,15 +10,15 @@ import { NotificationsMock } from './mock/notifications.mock';
 })
 export class NotificationService {
   private apiUrl = environment.apiUrl;
-  private mock = environment.apiUrl;
-  
+  private mockEnv = environment.apiUrl;
+
   private http: HttpClient = inject(HttpClient);
 
   private _notificationsSubject: BehaviorSubject<AppNotification[]> = new BehaviorSubject<AppNotification[]>([]);
   public notifications$ = this._notificationsSubject.asObservable();
 
-  fetchNotifications() {
-    if (this.mock) {
+  fetchNotifications(mock : boolean = true) {
+    if (this.mockEnv && mock) {
       this._notificationsSubject.next(NotificationsMock);
     } else {
       this.http.get<AppNotification[]>(`${this.apiUrl}/api/notifications`).subscribe(notifications => {
